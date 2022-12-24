fc57c142da218fcb0132e867e83199c53f6ce1a5
Evgeniy Solovev <73435568+djonnybravo@users.noreply.github.com>
2022-12-24 22:59:20 +0300

    test
    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Header from "./components/Header";
+import Technologies from "./components/Technologies";
 
 function App() {
   return (
     <div className="App">
-     Hello, samurai! Let's go!
+     <Header/>
+     <Technologies/>
     </div>
   );
 }
