ea23e02a462f85d009510e346be69f2d58aeda3a
Evgeniy Solovev <73435568+djonnybravo@users.noreply.github.com>
2022-12-24 22:54:28 +0300

    add some components

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+function Header() {
+    return (
+        <div>
+            <a href="#">One </a>
+            <a href="#">Two </a>
+            <a href="#">Three </a>
+            <a href="#">Four </a>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/components/Technologies.tsx
@@ -0,0 +1,14 @@
+function Technologies() {
+    return (
+        <div>
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JS</li>
+                <li>REACT</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Technologies
